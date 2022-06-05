1628b4a67b29466f525c474a7ae8c1dff896682f
Juan Gilsanz Polo <juangilsanz@gmail.com>
2022-06-05 20:40:43 +0200

    Added servers list

---
+++ lib/models/server.dart
@@ -0,0 +1,11 @@
+class Server {
+  final String ipAddress;
+  final String alias;
+  final String token;
+
+  const Server({
+    required this.ipAddress,
+    required this.alias,
+    required this.token
+  });
+}
--- lib/screens/servers.dart
+++ lib/screens/servers.dart
@@ -1,3 +1,4 @@
+import 'package:droid_hole/models/server.dart';
 import 'package:flutter/material.dart';
 
 class Servers extends StatelessWidget {
@@ -5,8 +6,102 @@ class Servers extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(child: Text("Servers"),),
+    double height = MediaQuery.of(context).size.height;
+    List servers = [
+      Server(ipAddress: '192.168.1.100', alias: 'Home', token: '123456'),
+      Server(ipAddress: '192.168.1.101', alias: 'Office', token: '123456'),
+      Server(ipAddress: '192.168.1.102', alias: 'Other', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+      Server(ipAddress: '192.168.1.103', alias: 'Server', token: '123456'),
+    ];
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(
+            top: 20,
+            left: 20,
+            right: 20,
+            bottom: 15
+          ),
+          child: Text(
+            "PiHole servers",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        ),
+        SizedBox(
+          height: height-143,
+          child: ListView.builder(
+            itemCount: servers.length,
+            itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 5
+              ),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const SizedBox(
+                        width: 48,
+                        child: Icon(Icons.storage_rounded),
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            servers[index].ipAddress,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            servers[index].alias,
+                            style: const TextStyle(
+                              fontSize: 14,
+                              fontStyle: FontStyle.italic
+                            ),
+                          )
+                        ],
+                      ),
+                      IconButton(
+                        onPressed: () => {}, 
+                        icon: const Icon(Icons.chevron_right)
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ),
+        ),
+      ],
     );
   }
 }
