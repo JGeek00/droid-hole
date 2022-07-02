52c88d9a0aa8db8c92f2280352b9bcb5133049ec
Juan Gilsanz Polo <juangilsanz@gmail.com>
2022-07-02 14:24:35 +0200

    Improved token modal

--- lib/widgets/token_modal.dart
+++ lib/widgets/token_modal.dart
@@ -31,13 +31,9 @@ class _TokenModalState extends State<TokenModal> {
 
   void _confirm() async {
     setState(() {
-      height = 370;
+      checkingToken = true;
+      height = 120;
     });
-    await Future.delayed(const Duration(milliseconds: 260), (() {
-      setState(() {
-        checkingToken = true;
-      });
-    }));
     final result = await testHash(widget.server, tokenTextController.text);
     if (result['result'] == 'success') {
       Navigator.pop(context);
@@ -45,15 +41,123 @@ class _TokenModalState extends State<TokenModal> {
     }
     else if (result['result'] == 'hash_not_valid') {
       setState(() {
-        tokenNotValid = true;
-        checkingToken = false;
         height = 300;
       });
+      await Future.delayed(const Duration(milliseconds: 260), (() {
+        setState(() {
+          tokenNotValid = true;
+          checkingToken = false;
+        });
+      }));
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget _form() {
+      return Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: Text(
+              "Token needed",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Enter here the API token.\n\nYou can get it on Settings > API/Web interface > Show API token.",
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    onChanged: (value) => setState(() {
+                      if (value != '') {
+                        textToken = true;
+                      }
+                      else {
+                        textToken = false;
+                      }
+                    }),
+                    controller: tokenTextController,
+                    enabled: checkingToken == false ? true : false,
+                    decoration: InputDecoration(
+                      label: const Text("API token"),
+                      border: const OutlineInputBorder(),
+                      errorText: tokenNotValid == true  
+                        ? "Token not valid"
+                        : null
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                TextButton.icon(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    widget.onCancel();
+                  },
+                  label: const Text("Cancel"),
+                  icon: const Icon(Icons.cancel),
+                ),
+                TextButton.icon(
+                  onPressed: textToken == true && checkingToken == false
+                    ? _confirm
+                    : null,
+                  style: ButtonStyle(
+                    overlayColor: MaterialStateProperty.all(
+                      Colors.green.withOpacity(0.1)
+                    ),
+                    foregroundColor: MaterialStateProperty.all(
+                      textToken == true && checkingToken == false
+                        ? Colors.green
+                        : Colors.grey,
+                    ),
+                  ), 
+                  label: Text(AppLocalizations.of(context)!.confirm),
+                  icon: const Icon(Icons.check)
+                ),
+              ],
+            ),
+          )
+        ],
+      );
+    }
+
+    Widget _checkingToken() {
+      return Padding(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            CircularProgressIndicator(),
+            SizedBox(width: 30),
+            Text(
+              "Checking token...",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
     return Dialog(
       backgroundColor: Theme.of(context).dialogBackgroundColor,
       shape: RoundedRectangleBorder(
@@ -63,103 +167,9 @@ class _TokenModalState extends State<TokenModal> {
         duration: const Duration(milliseconds: 250),
         curve: Curves.easeInOut,
         height: height,
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 20),
-              child: Text(
-                "Token needed",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18
-                ),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "Enter here the API token.\n\nYou can get it on Settings > API/Web interface > Show API token.",
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 20),
-                    TextFormField(
-                      onChanged: (value) => setState(() {
-                        if (value != '') {
-                          textToken = true;
-                        }
-                        else {
-                          textToken = false;
-                        }
-                      }),
-                      controller: tokenTextController,
-                      enabled: checkingToken == false ? true : false,
-                      decoration: InputDecoration(
-                        label: const Text("API token"),
-                        border: const OutlineInputBorder(),
-                        errorText: tokenNotValid == true  
-                          ? "Token not valid"
-                          : null
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            if (checkingToken == true) Padding(
-              padding: const EdgeInsets.all(20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  CircularProgressIndicator(),
-                  SizedBox(width: 30),
-                  Text(
-                    "Checking token...",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TextButton.icon(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                      widget.onCancel();
-                    },
-                    label: const Text("Cancel"),
-                    icon: const Icon(Icons.cancel),
-                  ),
-                  TextButton.icon(
-                    onPressed: textToken == true && checkingToken == false
-                      ? _confirm
-                      : null,
-                    style: ButtonStyle(
-                      overlayColor: MaterialStateProperty.all(
-                        Colors.green.withOpacity(0.1)
-                      ),
-                      foregroundColor: MaterialStateProperty.all(
-                        textToken == true && checkingToken == false
-                          ? Colors.green
-                          : Colors.grey,
-                      ),
-                    ), 
-                    label: Text(AppLocalizations.of(context)!.confirm),
-                    icon: const Icon(Icons.check)
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
+        child: checkingToken == true 
+          ? _checkingToken()
+          : _form()
       ),
     );
   }
