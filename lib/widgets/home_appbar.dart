497fecfa97f1275bf64f08ca511dabb9df3eddff
Juan Gilsanz Polo <juangilsanz@gmail.com>
2022-12-31 14:51:59 +0100

    Added fast server switch

--- lib/widgets/home_appbar.dart
+++ lib/widgets/home_appbar.dart
@@ -5,8 +5,10 @@ import 'package:flutter_web_browser/flutter_web_browser.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import 'package:droid_hole/widgets/switch_server_modal.dart';
 import 'package:droid_hole/screens/servers.dart';
 
+import 'package:droid_hole/models/server.dart';
 import 'package:droid_hole/config/system_overlay_style.dart';
 import 'package:droid_hole/providers/app_config_provider.dart';
 import 'package:droid_hole/classes/process_modal.dart';
@@ -23,7 +25,6 @@ class HomeAppBar extends StatelessWidget with PreferredSizeWidget {
     final appConfigProvider = Provider.of<AppConfigProvider>(context);
 
     final width = MediaQuery.of(context).size.width;
-    final statusBarHeight = MediaQuery.of(context).viewPadding.top;
 
     void refresh() async {
       final ProcessModal process = ProcessModal(context: context);
@@ -80,67 +81,123 @@ class HomeAppBar extends StatelessWidget with PreferredSizeWidget {
       }
     }
 
+    void connectToServer(Server server) async {
+      Future connectSuccess(result) async {
+        serversProvider.setselectedServer(Server(
+          address: server.address,
+          alias: server.alias,
+          token: server.token!,
+          defaultServer: server.defaultServer,
+          enabled: result['status'] == 'enabled' ? true : false
+        ));
+        serversProvider.setPhpSessId(result['phpSessId']);
+        final statusResult = await realtimeStatus(server, result['phpSessId']);
+        if (statusResult['result'] == 'success') {
+          serversProvider.setRealtimeStatus(statusResult['data']);
+        }
+        final overtimeDataResult = await fetchOverTimeData(server, result['phpSessId']);
+        if (overtimeDataResult['result'] == 'success') {
+          serversProvider.setOvertimeData(overtimeDataResult['data']);
+          serversProvider.setOvertimeDataLoadingStatus(1);
+        }
+        else {
+          serversProvider.setOvertimeDataLoadingStatus(2);
+        }
+        serversProvider.setIsServerConnected(true);
+        serversProvider.setRefreshServerStatus(true);
+      }
+
+      final ProcessModal process = ProcessModal(context: context);
+      process.open(AppLocalizations.of(context)!.connecting);
+
+      final result = await loginQuery(server);
+      process.close();
+      if (result['result'] == 'success') {
+        await connectSuccess(result);
+      }
+      else {
+        showSnackBar(
+          context: context, 
+          appConfigProvider: appConfigProvider,
+          label: AppLocalizations.of(context)!.cannotConnect,
+          color: Colors.red
+        );
+      }
+    }
+
+    void openSwitchServerModal() {
+      showDialog(
+        context: context, 
+        builder: (context) => SwitchServerModal(
+          onServerSelect: connectToServer,
+        )
+      );
+    }
+
     return AppBar(
       systemOverlayStyle: systemUiOverlayStyleConfig(context),
       toolbarHeight: 70,
       title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Row(
-            children: serversProvider.selectedServer != null 
-              ? [
-                  Icon(
-                    serversProvider.isServerConnected == true 
-                      ? serversProvider.selectedServer!.enabled == true 
-                        ? Icons.verified_user_rounded
-                        : Icons.gpp_bad_rounded
-                      : Icons.shield_rounded,
+          GestureDetector(
+            onTap: openSwitchServerModal,
+            child: Row(
+              children: serversProvider.selectedServer != null 
+                ? [
+                    Icon(
+                      serversProvider.isServerConnected == true 
+                        ? serversProvider.selectedServer!.enabled == true 
+                          ? Icons.verified_user_rounded
+                          : Icons.gpp_bad_rounded
+                        : Icons.shield_rounded,
+                      size: 30,
+                      color: serversProvider.isServerConnected == true 
+                        ? serversProvider.selectedServer!.enabled == true
+                          ? Colors.green
+                          : Colors.red
+                        : Colors.grey
+                    ),
+                    const SizedBox(width: 20),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          serversProvider.selectedServer!.alias,
+                          style: const TextStyle(
+                            fontSize: 20
+                          ),
+                        ),
+                        Text(
+                          serversProvider.selectedServer!.address,
+                          style: const TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14
+                          )
+                        )
+                      ],
+                    ),
+                  ]
+                : [
+                  const Icon(
+                    Icons.shield,
+                    color: Colors.grey,
                     size: 30,
-                    color: serversProvider.isServerConnected == true 
-                      ? serversProvider.selectedServer!.enabled == true
-                        ? Colors.green
-                        : Colors.red
-                      : Colors.grey
                   ),
                   const SizedBox(width: 20),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        serversProvider.selectedServer!.alias,
-                        style: const TextStyle(
-                          fontSize: 20
-                        ),
+                  SizedBox(
+                    width: width - 128,
+                    child: Text(
+                      AppLocalizations.of(context)!.noServerSelected,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
                       ),
-                      Text(
-                        serversProvider.selectedServer!.address,
-                        style: const TextStyle(
-                          color: Colors.grey,
-                          fontSize: 14
-                        )
-                      )
-                    ],
-                  ),
-                ]
-              : [
-                const Icon(
-                  Icons.shield,
-                  color: Colors.grey,
-                  size: 30,
-                ),
-                const SizedBox(width: 20),
-                SizedBox(
-                  width: width - 128,
-                  child: Text(
-                    AppLocalizations.of(context)!.noServerSelected,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-              ]
+                ]
+            ),
           ),
         ],
       ),
---
+++ lib/widgets/switch_server_modal.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'package:droid_hole/models/server.dart';
+import 'package:droid_hole/providers/servers_provider.dart';
+
+class SwitchServerModal extends StatelessWidget {
+  final void Function(Server) onServerSelect;
+
+  const SwitchServerModal({
+    Key? key,
+    required this.onServerSelect
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final serversProvider = Provider.of<ServersProvider>(context);    
+
+    return AlertDialog(
+      scrollable: true,
+      title: Column(
+        children: [
+          const Icon(
+            Icons.storage_rounded,
+            size: 24,
+          ),
+          const SizedBox(height: 16),
+          Text(AppLocalizations.of(context)!.switchServer)
+        ],
+      ),
+      content: SizedBox(
+        width: double.maxFinite,
+        height: serversProvider.getServersList.length*72,
+        child: ListView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: serversProvider.getServersList.length,
+          itemBuilder: (context, index) => Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: () {
+                Navigator.pop(context);
+                onServerSelect(serversProvider.getServersList[index]);
+              },
+              child: ListTile(
+                title: Text(
+                  serversProvider.getServersList[index].alias,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                subtitle: Text(
+                  serversProvider.getServersList[index].address,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ),
+          )
+        ),
+      ),
+      actions: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            TextButton(
+              onPressed: () => Navigator.pop(context), 
+              child: Text(AppLocalizations.of(context)!.cancel)
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
