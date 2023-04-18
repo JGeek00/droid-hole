c8829be2ec5792cdab24b0d822f4566c7d1e2c96
Juan Gilsanz Polo <juangilsanz@gmail.com>
2023-04-18 22:50:15 +0200

    Added possible regex for domains

--- lib/screens/domains/add_domain_modal.dart
+++ lib/screens/domains/add_domain_modal.dart
@@ -55,6 +55,7 @@ class _AddDomainModalState extends State<AddDomainModal> {
 
   void validateDomain(String? value) {
     if (value != null && value != '') {
+      // Possible regexp with support for *. and one letter domain: ^(\*\.)?([a-zA-Z0-9-_]+\.)*([a-zA-Z0-9-_])+\.[a-z]+$
       RegExp subrouteRegexp = RegExp(r'^(([a-zA-Z]{1})|([a-zA-Z]{1}[a-zA-Z]{1})|([a-zA-Z]{1}[0-9]{1})|([0-9]{1}[a-zA-Z]{1})|([a-zA-Z0-9][a-zA-Z0-9-_]{1,61}[a-zA-Z0-9]))\.([a-zA-Z]{2,6}|[a-zA-Z0-9-]{2,30}\.[a-zA-Z]{2,3})$');
       if (subrouteRegexp.hasMatch(value) == true) {
         setState(() {
